c2d4a920f979ec6684f92adcb282965629e6f131
Viollo <vadim.siraev@bk.ru>
2018-11-05 17:06:43 +0300

    Добавьте файлы проекта.

---
+++ GeometryApp/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryApp
+{
+        public struct Point
+        {
+            public int X;
+            public int Y;
+
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public void Move(bool isX, int value)
+            {
+                if (isX)
+                {
+                    X += value;
+                }
+                else
+                {
+                    Y += value;
+                }
+            }
+        }
+        public struct Line
+        {
+            public Point StartPoint;
+            public Point EndPoint;
+
+            public Line(Point startPoint, Point endPoint)
+            {
+                StartPoint = startPoint;
+                EndPoint = endPoint;
+            }
+            public double GetLength()
+            {
+                return Math.Sqrt(Math.Pow(EndPoint.X - StartPoint.X, 2) + Math.Pow(EndPoint.Y - StartPoint.Y, 2));
+            }
+        }
+        class Program
+        {
+            public static int AdvancedParse(string label)
+            {
+                int a;
+                while (true)
+                {
+                    Console.WriteLine($"Введите {label}");
+                    if (int.TryParse(Console.ReadLine(), out a))
+                    {
+                        return a;
+                    }
+                    Console.WriteLine("Некоректный ввод");
+                }
+
+
+            }
+            static void Main(string[] args)
+            {
+                int x1 = AdvancedParse("x1");
+                int y1 = AdvancedParse("y1");
+                int x2 = AdvancedParse("x2");
+                int y2 = AdvancedParse("y2");
+
+                Point startPoint = new Point(x1, y1);
+                Point endPoint = new Point(x2, y2);
+                Line line = new Line(startPoint, endPoint);
+
+                while (true)
+                {
+                    Console.WriteLine("\nВыбирете действие:\n 1. Узнать длину отрезка \n 2. Изменить координаты точек \n Для завершения работы нажмите Ecs");
+                    var Button = Console.ReadKey().Key;
+                    switch (Button)
+                    {
+                        case ConsoleKey.D1:
+                            Console.WriteLine($"\n Длина отрезка = {line.GetLength()}");
+                            break;
+                        case ConsoleKey.D2:
+                            break;
+                        case ConsoleKey.Escape:
+                            System.Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("Некоректный ввод");
+                            continue;
+                    }
+                }
+            }
+        }
+}
---
+++ GeometryApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("GeometryApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("GeometryApp")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("2c0b7794-2fb7-4fde-a3e2-342ff40ce4fa")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
